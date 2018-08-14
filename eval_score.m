b7777d027df996cf49521bd5dd422d64c6c94c6c
leon <18850544602@163.com>
2018-08-14 14:29:11 +0800

    update evaluation file

---
+++ eval_score.m
@@ -0,0 +1,21 @@
+function [ evaluations ] = eval_score(split,ScoreMatrix,use_cdf,use_prc)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+load('splits_uniform.mat')
+evaluations = cell(3,2);
+evaluations{1,1} = 'cdf';
+evaluations{2,1} = 'precision';
+evaluations{3,1} = 'recall';
+if use_cdf
+    cdf_rates = cdf(full(splits{split}), ScoreMatrix,100);
+    evaluations{1,2} = cdf_rates;
+end
+if use_prc
+    rates = recall(full(splits{split}), ScoreMatrix,100) .*100 ;
+    pres = precision(full(splits{split}), ScoreMatrix,100) .* 100;
+    evaluations{2,2} = rates;
+    evaluations{3,2} = pres;   
+end
+
+end
+
